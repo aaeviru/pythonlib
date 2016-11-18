d9fd5c3c0d3971aaf0873ae7419744a4895c466e
aaeviru <aaeviru@gmail.com>
2016-11-18 06:57:12 +0000

    11/4

--- semantic.py
+++ semantic.py
@@ -6,9 +6,10 @@ import sys
 import math
 import re
 import numpy as np
+from scipy import spatial
 
 classname = '/home/ec2-user/git/tfidf/result/classname.txt'#literate classname
-
+alpha = 0.1 #parameter for rd
 
 def vecof0(lines,a,s,wtol,kk):
     if s == None:
@@ -38,7 +39,8 @@ def vecof3(lines,a,s,wtol,kk):#cal domain class of terms using lda
     vec = 1
     for line in lines:
         line = line.strip('\n')
-        vec = vec * a[:,wtol[line]] * 62300
+        if line in wtol:
+            vec = vec * a[:,wtol[line]] * 62300
     vec = vec * s * 100000
     return vec
 
@@ -97,6 +99,11 @@ def readcll2(cllfile):
     return cll
 
 
+def classof0(lines,a,s,wtol,kk):
+    if s == None:
+        return classof(lines,a,wtol,kk)
+    else:
+        return classof2(lines,a,s,wtol,kk)
 
 def classof(lines,a,wtol,kk):#cal domain class of terms using lsa
     vec = np.zeros(kk)
@@ -109,9 +116,19 @@ def classof2(lines,a,s,wtol,kk):#cal domain class of terms using lda
     vec = 1
     for line in lines:
         line = line.strip('\n')
-        vec = vec * a[:,wtol[line]]
+        if line in wtol:
+            vec = vec * a[:,wtol[line]]
     vec = vec * s
     return vec.argmax()
+
+def simcheck(q,P):
+    d = 2
+    for p in P:
+        sys.stdout.flush()
+        tmpd = spatial.distance.cosine(p,q)
+        if tmpd < d :
+            d = tmpd
+    return d
 
 def dg(filename,cll,clpath,a = None,s = None,wtol = None,kk = None,zipf = 1.03,type = 0):
 #dummpy query generation using tfidf
@@ -134,7 +151,7 @@ def dg(filename,cll,clpath,a = None,s = None,wtol = None,kk = None,zipf = 1.03,t
 	if type == 1:#tfidf2
 	    clf = clf + '2'
     elif type == 2 or type == 3:#lsa,lda
-        cl = classof(lines,a,wtol,kk)
+        cl = classof0(lines,a,s,wtol,kk)
 	clf = clpath+'/'+str(cl)
 
     w = []
@@ -157,12 +174,11 @@ def dg(filename,cll,clpath,a = None,s = None,wtol = None,kk = None,zipf = 1.03,t
     del tmp
     for tcl in cll[cl]:
         if type == 0 or type == 1:
-            clf = clpath+'/'+cl[0]+'/'+cl+'.txt.fq.tfidfn'
+            clf = clpath+'/'+tcl[0]+'/'+tcl+'.txt.fq.tfidfn'
             if type == 1:
                 clf = clf + '2'
         if type == 2 or type == 3:
-            cl = classof(lines,a,wtol,kk)
-            clf = clpath+'/'+str(cl)
+            clf = clpath+'/'+str(tcl)
 	fcl = open(clf,'r')
 	tmp = fcl.readlines()[0:10000]
 	fcl.close()
@@ -184,6 +200,91 @@ def dg(filename,cll,clpath,a = None,s = None,wtol = None,kk = None,zipf = 1.03,t
 		else:
 		    continue
         result.append(list(qtem))
+    qtem = []
+    for i in w:
+	tw = ttmmpp[i].strip('\n')
+	qtem.append(tw)
+	t = t + tw + ' '
+    result.append(list(qtem))
+    result.append(t)
+    return result
+
+
+def dg2(filename,cll,clpath,a = None,s = None,wtol = None,kk = None,zipf = 1.03,P = None,type = 0):
+#dummpy query generation using tfidf
+#type:0 tfidf/1 tfidf2/2 lsa/3 lda
+#s for lda only
+    fin  = open(filename+'.txt','r')
+    lines = fin.readlines()
+    fin.close()
+    if type == 0 or type == 1:#tfidf
+        fin = open(filename,'r')
+        temp = fin.read()
+        fin.close()
+        cl = re.findall(r'【国際特許分類第.*版】.*?([A-H][0-9]+?[A-Z])',temp,re.DOTALL)
+        if(len(cl) < 1):
+            print 'cl<1:',filename
+            return None
+        cl = cl[0]
+        cl = cl[0] + str(int(cl[1:len(cl)-1])) +cl[len(cl)-1]
+	clf = clpath+'/'+cl[0]+'/'+cl+'.txt.fq.tfidfn'
+	if type == 1:#tfidf2
+	    clf = clf + '2'
+    elif type == 2 or type == 3:#lsa,lda
+        cl = classof0(lines,a,s,wtol,kk)
+	clf = clpath+'/'+str(cl)
+
+    w = []
+    fcl = open(clf,'r')
+    tmp = fcl.readlines()
+    fcl.close()
+    for line in lines:
+	if line in tmp[0:10000]:
+	    w.append(tmp.index(line))
+    rd = simcheck(vecof0(lines,a,s,wtol,kk),P)
+    rd = abs(rd + alpha * (np.random.random_sample() - 0.5))
+    r = []
+    t = '!'
+    result = []
+    if cl not in cll:
+        return None
+    ttmmpp = list(tmp)
+    del tmp
+    for tcl in cll[cl]:
+        beta = alpha
+        if type == 0 or type == 1:
+            clf = clpath+'/'+tcl[0]+'/'+tcl+'.txt.fq.tfidfn'
+            if type == 1:
+                clf = clf + '2'
+        if type == 2 or type == 3:
+            clf = clpath+'/'+str(tcl)
+	fcl = open(clf,'r')
+	tmp = fcl.readlines()[0:10000]
+	fcl.close()
+	#qlen = abs(len(w)+np.random.normal(0,2,1)) #random querry length
+	qlen = len(w)
+        sc = False
+        it = 0 
+        while sc == False:
+            it = it + 1
+            if it > 1000:
+                it = 1
+                beta = beta * 2
+            rr =  set()
+            qtem = []
+            while len(rr) < qlen:
+                dp = int(np.random.zipf(zipf,1))
+                if dp < len(tmp) and dp not in rr:
+                    rr.add(dp)
+                    qtem.append(tmp[int(dp)].strip('\n'))
+                else:
+                    continue
+            stm = simcheck(vecof0(qtem,a,s,wtol,kk),P)
+            #print it,rd,stm
+            if  abs(stm - rd) < beta * 0.5:
+                sc = True
+        result.append(list(qtem))
+    qtem = []
     for i in w:
 	tw = ttmmpp[i].strip('\n')
 	qtem.append(tw)
--- sysf.py
+++ sysf.py
@@ -3,13 +3,15 @@ import os
 from time import gmtime, strftime
 
 def logger(outf,inputform):
+    if outf[0:6] == 'stdout':
+        return sys.stdout
     outft = outf
     i = 0
     while os.path.isfile(outf+'.txt'):
 	i = i + 1
 	outf = outft+'-v'+str(i)
-    fout = open(outf+'.txt','w')
-    flog = open(outf+'.log', 'w')
+    fout = open(outf+'.txt','w',0)
+    flog = open(outf+'.log', 'w',0)
     sys.stdout = flog
     sys.stderr = flog
 
@@ -18,6 +20,7 @@ def logger(outf,inputform):
     for i in range(1,len(sys.argv)):
 	print inputform[i-1]+':'+sys.argv[i]
     print
+    sys.stdout.flush()
     return fout
 
 
