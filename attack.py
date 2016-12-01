278ab04e3303eb8f8ba5ee3d776e3c9776746c89
aaeviru <aaeviru@gmail.com>
2016-12-01 14:35:04 +0000

    11/29

--- attack.py
+++ attack.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+def coef(p,q):
+    tmp = len([val for val in p if val in q])
+    return 2.0*tmp/(len(p)+len(q))
 
-def mtatt():
+
+def simatt(q,Pu,alpha = 0.5):
+    cc = []
+    for p in Pu:
+        cc.append(coef(p,q))
+    cc = sorted(cc)
+    for i in range(1,len(cc)):
+        cc[i] = alpha * cc[i] + (1-alpha) * cc[i-1]
+    return cc[-1]
+    
+
     
